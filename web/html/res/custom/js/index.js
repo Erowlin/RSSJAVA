1756718943d7dadf106d2b896ac943500f7e12b0
Thomas Romera <github@thomas-romera.com>
2014-09-27 16:56:10 +0200

    Changed the behaviour on click js event

--- web/html/res/custom/js/index.js
+++ web/html/res/custom/js/index.js
@@ -2,13 +2,8 @@ YUI().use('node', function(Y) {
     Y.delegate('click', function(e) {
         var idFeedItem = e.currentTarget.getAttribute('feed-item-id');
 
-        console.log(idFeedItem);
 
-        nodes = Y.all("[feed-item-id-hide="+ idFeedItem+"]");
-        console.log(nodes);
-        nodes.each(function (tata){
-        	console.log(tata);
-        	tata.toggleView();
-        });
+        node = Y.one("[feed-item-id-hide="+ idFeedItem+"]");
+        node.toggleView();
     }, document, '.feed-content-title');
 });

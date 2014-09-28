eca6272ba141debaa17186788bf9ee90ab14c5b2
BaptisteLedoux <b.ledoux.epitech@gmail.com>
2014-09-28 16:28:11 +0200

    build entries list dynamically (ng-repeat)

--- web/angular/app/view_main/view_main.js
+++ web/angular/app/view_main/view_main.js
@@ -2,34 +2,60 @@
 
 angular.module('myApp.view_main', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view_main', {
-    templateUrl: 'view_main/view_main.html',
-    controller: 'View_mainCtrl'
-  });
-}])
+.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/view_main', {
+            templateUrl: 'view_main/view_main.html',
+            controller: 'View_mainCtrl'
+        });
+    }
+])
 
-.controller('View_mainCtrl', [function() {
-    
-    console.log("totot");
-    $('.nav-menu-button').click(function(e) {
-        e.preventDefault();
-        if ($('#nav').hasClass('active')) {
-            $('.nav-inner').slideToggle(function() {
-                $('#nav').toggleClass('active');
+.controller('View_mainCtrl', [
+
+        function() {
+
+            console.log("Entering view main controller..");
+
+            $('.nav-menu-button').click(function(e) {
+                e.preventDefault();
+                if ($('#nav').hasClass('active')) {
+                    $('.nav-inner').slideToggle(function() {
+                        $('#nav').toggleClass('active');
+                    });
+                } else {
+                    $('.nav-inner').slideToggle();
+                    $('#nav').toggleClass('active');
+                }
+
             });
-        } else {
-            $('.nav-inner').slideToggle();
-            $('#nav').toggleClass('active');
+
+            
+
+        }]);
+
+    function entriesController($scope) {
+        // Defining example datas
+        $scope.listEntry = [{
+            id: '1',
+            title: 'Welcome to Toronto',
+            date: 'Posted at 3:56pm, April 3, 2012',
+            description: 'This is an example'
+        }, {
+            id: '2',
+            title: 'Welcome to Beijing',
+            date: 'Posted at 3:00pm, April 4, 2012',
+            description: 'This is an example'
+        }, {
+            id: '3',
+            title: 'Welcome to Paris',
+            date: 'Posted at 3:56pm, April 5, 2012',
+            description: 'This is an example'
+        }, ];
+
+        $scope.slideContent = function(id) {
+            console.log('id: e_' + id);
+
+            $('#e_' + id).slideToggle();
         }
-
-    });
-
-    $('#toronto').click(function() {
-        $('#content_toronto').slideToggle();
-    });
-
-}]);
-
-
-
+    }
